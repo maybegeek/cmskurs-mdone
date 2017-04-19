ee10fc1482e62ab1ee7172ddf5d79241c9084f1a
maybegeek <chr.pfeiffer@gmail.com>
2017-04-20 00:22:38 +0200

    init session, lang variables, testing with footer content
    session mit Möglichkeit der Sprachumschaltung:
    
    .../?lang=DE | .../?lang=EN
    
    Variablen zu Metaangaben in der index.php, könnten auch ausgelagert werden.

--- index.php
+++ index.php
@@ -1,16 +1,45 @@
 <?php
+session_start();
+header('Cache-control: private');
+// multilanguage
+$languages = array('DE', 'EN');
+// Parsedown for Markdown to HTML
 include('assets/parsedown/Parsedown.php');
 //
-$tmplfooter = file_get_contents('./inhalt/footer-de.md');
+if(in_array($_GET['lang'], $languages)) {
+  $_SESSION['lang'] = $_GET['lang'];
+  header('Location:http://maybegeek.de/~cms/mdone/');
+  exit();
+}
+
+define('LANG', in_array($_SESSION['lang'], $languages) ? $_SESSION['lang'] : 'DE');
+$i18n = array();
+$i18n['short'] = strtolower(LANG);
+
+// technical debt: statische Sprach-String-Zuweisung
+$i18n['DE']['title'] = 'mdone | markdown one page';
+$i18n['EN']['title'] = 'mdone | markdown one page';
+$i18n['DE']['metaDescription'] = 'mdone | markdown für one-pager';
+$i18n['EN']['metaDescription'] = 'mdone | markdown one page';
+$i18n['DE']['metaKeywords'] = 'markdown, parsedown, multilanguage, static site';
+$i18n['EN']['metaKeywords'] = 'markdown, parsedown, multilanguage, static site';
+$i18n['DE']['metaAuthor'] = 'Vorname Nachname';
+$i18n['EN']['metaAuthor'] = 'Vorname Nachname';
+
+// get the language corresponding content
+$tmplfooter = file_get_contents("./inhalt/footer-{$i18n['short']}.md");
+
+// init Parsedown
 $Parsedown = new Parsedown();
 ?><!DOCTYPE html>
-<html lang="de">
+<html lang="<?php echo $i18n['short']; ?>">
   <head>
     <meta charset="utf-8">
-    <title>mdone | markdown one page</title>
-    <meta name="author" content="Vorname Nachname">
-    <meta name="keywords" content="Begriff1, Begriff2, Begriff3">
-    <meta name="description" content="Beschreibung des Themas der Seite">
+    <title><?php echo $i18n[LANG]['title']; ?></title>
+    <meta name="description" content="<?php echo $i18n[LANG]['metaDescription']; ?>">
+    <meta name="keywords" content="<?php echo $i18n[LANG]['metaKeywords']; ?>">
+    <meta name="author" content="<?php echo $i18n[LANG]['metaAuthor']; ?>">
+    <meta name="publisher" content="<?php echo $i18n[LANG]['metaAuthor']; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://rawcdn.githack.com/maybegeek/neogridic/master/neogridic.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
