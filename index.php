59a6ab15d465cfd7750b925f761e7f2f01d8faf7
maybegeek <chr.pfeiffer@gmail.com>
2017-04-19 17:04:14 +0200

    simple template, added css to assets
    Template etwas den Überlegungen angepasst. CSS dazu, Semantik, ...

--- index.php
+++ index.php
@@ -1,6 +1,51 @@
 <?php
 include('assets/parsedown/Parsedown.php');
+//
+//$Parsedown = new Parsedown();
+//echo $Parsedown->text( $blubber );
+?><!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>mdone | markdown one page</title>
+    <meta name="author" content="Vorname Nachname">
+    <meta name="keywords" content="Begriff1, Begriff2, Begriff3">
+    <meta name="description" content="Beschreibung des Themas der Seite">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://rawcdn.githack.com/maybegeek/neogridic/master/neogridic.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
 
-$Parsedown = new Parsedown();
-echo $Parsedown->text('# Dinge und Sachen');
-?>
+    <header class="tmpl-header">
+      <h1>mdone</h1>
+      <p>markdown to one page</p>
+    </header>
+
+    <nav class="tmpl-nav">
+      <ul>
+        <li><a href="#">Dinge</a></li>
+        <li><a href="#">Sachen</a></li>
+        <li><a href="#">Zeug</a></li>
+      </ul>
+    </nav>
+
+    <main>
+      <section class="tmpl-main-one">
+
+      </section>
+
+      <section class="tmpl-main-two">
+
+      </section>
+
+      <section class="tmpl-main-three">
+
+      </section>
+    </main>
+
+    <footer class="tmpl-footer">
+
+    </footer>
+  </body>
+</html>
