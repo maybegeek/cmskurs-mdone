d69cc3daa996547d5074ba1d262805a94807d48b
maybegeek <chr.pfeiffer@gmail.com>
2017-04-20 09:28:35 +0200

    more/better i18n, grid width set to max 640px

--- index.php
+++ index.php
@@ -17,12 +17,14 @@ $i18n = array();
 $i18n['short'] = strtolower(LANG);
 
 // technical debt: statische Sprach-String-Zuweisung
-$i18n['DE']['title'] = 'mdone | markdown one page';
-$i18n['EN']['title'] = 'mdone | markdown one page';
-$i18n['DE']['metaDescription'] = 'mdone | markdown für one-pager';
-$i18n['EN']['metaDescription'] = 'mdone | markdown one page';
-$i18n['DE']['metaKeywords'] = 'markdown, parsedown, multilanguage, static site';
-$i18n['EN']['metaKeywords'] = 'markdown, parsedown, multilanguage, static site';
+$i18n['DE']['title'] = 'mdone';
+$i18n['EN']['title'] = 'mdone';
+$i18n['DE']['teaser'] = 'Markdown Ein-Seiter';
+$i18n['EN']['teaser'] = 'markdown one pager';
+$i18n['DE']['metaDescription'] = 'mdone | Markdown plus Parsedown = Ein-Seiter';
+$i18n['EN']['metaDescription'] = 'mdone | markdown, parsedown, one page layout';
+$i18n['DE']['metaKeywords'] = 'Markdown, Parsedown, Multilanguage, Ein-Seiter';
+$i18n['EN']['metaKeywords'] = 'markdown, parsedown, multilanguage, one page';
 $i18n['DE']['metaAuthor'] = 'Vorname Nachname';
 $i18n['EN']['metaAuthor'] = 'Vorname Nachname';
 
@@ -38,7 +40,7 @@ $Parsedown = new Parsedown();
 <html lang="<?php echo $i18n['short']; ?>">
   <head>
     <meta charset="utf-8">
-    <title><?php echo $i18n[LANG]['title']; ?></title>
+    <title><?php echo $i18n[LANG]['title']; ?> | <?php echo $i18n[LANG]['teaser']; ?></title>
     <meta name="description" content="<?php echo $i18n[LANG]['metaDescription']; ?>">
     <meta name="keywords" content="<?php echo $i18n[LANG]['metaKeywords']; ?>">
     <meta name="author" content="<?php echo $i18n[LANG]['metaAuthor']; ?>">
@@ -47,11 +49,11 @@ $Parsedown = new Parsedown();
     <link rel="stylesheet" href="https://rawcdn.githack.com/maybegeek/neogridic/master/neogridic.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
-  <body class="grid w960">
+  <body class="grid w640">
 
     <header class="tmpl-header row">
-      <h1 class="c8">mdone</h1>
-      <p class="c4">markdown to one page</p>
+      <h1 class="c8"><?php echo $i18n[LANG]['title']; ?></h1>
+      <p class="c4"><?php echo $i18n[LANG]['teaser']; ?></p>
     </header>
 
     <nav class="tmpl-nav row">
